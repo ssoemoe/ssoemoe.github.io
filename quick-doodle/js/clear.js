fa68669e158dacf532f985f90c44b86e671e49a7
Shane Moe <ssoemoe@terpmail.umd.edu>
2020-04-26 13:49:23 -0400

    :sparkles: feat: touchscreen enabled

--- quick-doodle/js/clear.js
+++ quick-doodle/js/clear.js
@@ -1,6 +1,8 @@
 var clearBtn = document.querySelector("#clear-btn");
 clearBtn.addEventListener('click', function (event) {
     context.clearRect(0, 0, canvas.width, canvas.height);
+    actionHistory.push(recent);
+    recent = [];
     actionHistory.push([
         {
             action: "clear"
